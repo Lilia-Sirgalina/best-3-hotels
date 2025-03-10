1ac03009b843a27ef958fc4f9bd2c0fde285029c
Lilia-Sirgalina <lilia_developer@hotmail.com>
2025-03-10 14:25:02 +0100

    Changes in CSS

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,76 @@ import './App.css'
 
 function App() {
 
-  const[hotels, setHotels] = useState(data);
-  console.log(hotels)
+  const[hotels, setHotels] = useState(0);
+  const {id, hotel, image, description, showMore, sourse} = data[hotels];  
+
+  const previous = () => {
+    setHotels(hotels => {
+      hotels--;
+      if(hotels<0) {
+      hotels = data.length-1;
+    }
+    return hotels;
+  })  
+  }
+
+  const next = () => {
+    setHotels(hotels => {
+      hotels++;
+      if(hotels>data.length-1) {
+      hotels = 0;
+    }
+    return hotels;
+  })  
+  }
   
 
   return (
     <>
+    <div className='header'>
+      <h1>HOTELES CON ENCANTO EN CATALUÑA</h1>
+    </div>
+
+    <div className='header'>
+        <h2>Lista de Hoteles</h2>
+    </div>
+
     <div className='container'>
+
+      <div className='line'>
+        <h3>{id} - {hotel}</h3>
+      </div>
+
+      <div className='image'>
+        <img src={image} alt="hotel picture" width="500px" />
+      </div>
+
+      <div className='description'>
+        <p>{description}</p>
+        <p>Reservar: <a href={sourse} target='_blank'>pagina web del hotel</a></p>
+      </div>
+
+      <div className='buttons'>
+        <button className='btn' onClick={previous}>Anterior</button>
+        <button className='btn'>Añadir al TOP-3</button>
+        <button className='btn' onClick={next}>Siguiente</button>
+      </div>
+      
+      
+    </div>
+    
+
+
+    
+     
+    </>
+  )
+}
+
+export default App
+
+
+{/* <div className='container'>
       <h1>HOTELES CON ENCANTO EN CATALUÑA</h1>
     </div>
 
@@ -24,23 +87,14 @@ function App() {
           <h2>{hotel}</h2>
           <h2>{stars}</h2>
           <img src={image} alt="hotel room" width="500px" />
-          <p>{description}</p>          
+          <p>{description}</p>
+          <p>Reservar: <a href={sourse}>pagina web del hotel</a></p>          
         </div>
 
-        <div className='button'>   
-          <a href={sourse}>RESERVAR</a>       
-          <button>ELIMINAR</button>
+        <div className='button'>
+          <button>AÑADIR A TOP 3</button>          
         </div>
 
       </div>
       )
-    })}
-
-
-    
-     
-    </>
-  )
-}
-
-export default App
+    })} */}
--- src/data.jsx
+++ src/data.jsx
@@ -3,15 +3,15 @@
 export const data = [
     {
         id: 1,        
-        hotel: "Petit mirador",
+        hotel: "Petit mirador (Torrellas de Llobregat)",
         image: "https://webbox.imgix.net/images/xnlqhdilkviknnie/5d2c590f-277b-4285-a76c-688c4ef4a167.jpg?auto=format,compress&fit=crop&crop=entropy&w=1920",
-        description: "El Petit Mirador, es un hotel de 3* con encanto, situado a tan solo 20 min de Barcelona, y del aeropuerto del Prat. Cuenta con 6 amplias habitaciones con baño, Tv, a.a, calefacción, wifi. El hotel dispone de zonas de ocio como sala de juegos, billar y salón de TV. Hay conexión Wi-Fi gratuita en todo el hotel para su comodidad. En la terraza hay unas vistas maravillosas que no olvidará fácilmente. También disponemos de piscina y jacuzzi exterior panorámico rodeado de tumbonas ideal para relajarse y disfrutar en verano. Si lo desea, podrá solicitar al personal una sesión de masaje. El Petit Mirador se encuentra en el corazón de la comarca del Baix Llobregat. Está a solo 8 km de la autopista A-2 y a apenas 6 km de la famosa Colonia Güell de Gaudí. Muy cerca encontrará el popular parque de Cataluña en Miniatura, con descuentos para los huéspedes del hotel.",
+        description: "El Petit Mirador, es un hotel de 3* con encanto, situado a tan solo 20 min de Barcelona, y del aeropuerto del Prat. Cuenta con 6 amplias habitaciones con baño, Tv, a.a, calefacción, wifi. El hotel dispone de zonas de ocio como sala de juegos, billar y salón de TV. Hay conexión Wi-Fi gratuita en todo el hotel para su comodidad. En la terraza hay unas vistas maravillosas que no olvidará fácilmente. También disponemos de piscina y jacuzzi exterior panorámico rodeado de tumbonas ideal para relajarse y disfrutar en verano. Si lo desea, podrá solicitar al personal una sesión de masaje. El Petit Mirador se encuentra en el corazón de la comarca del Baix Llobregat.",
         showMore: false,
         sourse: "https://www.petitmirador.com/es/index.html"
     }, 
     {
         id: 2,        
-        hotel: "Milestrelles",
+        hotel: "Milestrelles (Cornellà del Terri)",
         image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/376573408.jpg?k=c2bca3fada55551fd3d39747750d44d02b3dd23a017074ca23d2fab688ba7bef&o=&hp=1",
         description: "El hotel Milestrelles encabeza el ranking de hoteles originales en Cataluña. Es el alojamiento más popular en el que disfrutar de una noche bajo las estrellas. Sus conocidas habitaciones burbuja con el techo transparente, hacen que puedas disfrutar de unas magníficas vistas a las estrellas. Eso sí, debido a su gran popularidad, es muy complicado conseguir habitación en este hotel con encanto, ¡Así que te aconsejo que reserves tu estancia con tiempo!",
         showMore: false, 
@@ -19,7 +19,7 @@ export const data = [
     },
     {
         id: 3,        
-        hotel: "Iglú de las Estrellas",
+        hotel: "Iglú de las Estrellas (Xalet de Prades)",
         image: "https://static.wixstatic.com/media/8b3917_ac27537e38264b0bb6de913fa7c8039d~mv2.jpg/v1/fill/w_953,h_594,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/8b3917_ac27537e38264b0bb6de913fa7c8039d~mv2.jpg",
         description: "El Iglú de les Estrelles, del Xalet de Prades, es otro de los hoteles originales en Cataluña para ver las estrellas. En este caso, la burbuja es una estructura fija que solo tiene el techo transparente para poder contemplar las estrellas desde la cama. También tiene una bañera dentro de la habitación para poder disfrutar de un momento de relax mirando el cielo estrellado. Es una alternativa al Milestrelles menos conocida. Por lo que suele haber más disponibilidad y los precios son un poco más asequibles en temporada alta.",
         showMore: false, 
@@ -27,7 +27,7 @@ export const data = [
     },
     {
         id: 4,        
-        hotel: "Bubble Suite",
+        hotel: "Bubble Suite (Canyelles)",
         image: "https://chequealo.es/wp-content/uploads/FireShot-Capture-343-%F0%9D%97%95%F0%9D%98%82%F0%9D%97%AF%F0%9D%97%AF%F0%9D%97%B9%F0%9D%97%B2-%F0%9D%98%80%F0%9D%98%82%F0%9D%97%B6%F0%9D%98%81%F0%9D%97%B2%F0%9D%98%80-by-%F0%9D%97%A3%F0%9D%97%B9%F0%9D%97%AE-%F0%9D%97%B1%F0%9D%97%B2%F0%9D%97%B9-%F0%9D%97%95%F0%9D%97%BC%F0%9D%98%80%F0%9D%97%B0-@masiapladelbosc_-www.jpg",
         description: "Siguiendo en la línea de dormir bajo las estrellas, encontramos las Bubble Suite de la Masia Pla del Bosc, un alojamiento diferente en el Garraf. Se trata de un alojamiento menos conocido, puesto que su apertura es bastante reciente, pero no tiene nada que envidiarle al resto de hoteles originales de la lista. Lo más llamativo de este alojamiento es que dispone de un jacuzzi privado en la misma burbuja y de una terraza preciosa para disfrutar del entorno natural.",
         showMore: false, 
@@ -35,7 +35,7 @@ export const data = [
     },
     {
         id: 5,        
-        hotel: "Les Cols Pavellons",
+        hotel: "Les Cols Pavellons (Olot)",
         image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/481186205.jpg?k=b61cd3b3a34b17e3fa38d9562efbfef3be1a870050eb54272a92aa4fca8505d2&o=&hp=1",
         description: "Siguiendo en la línea de dormir bajo las estrellas, encontramos las Bubble Suite de la Masia Pla del Bosc, un alojamiento diferente en el Garraf. Se trata de un alojamiento menos conocido, puesto que su apertura es bastante reciente, pero no tiene nada que envidiarle al resto de hoteles originales de la lista. Lo más llamativo de este alojamiento es que dispone de un jacuzzi privado en la misma burbuja y de una terraza preciosa para disfrutar del entorno natural.",
         showMore: false, 
@@ -43,7 +43,7 @@ export const data = [
     },
     {
         id: 6,        
-        hotel: "Cabanes als arbres",
+        hotel: "Cabanes als arbres (Sant Hilari Sacalm)",
         image: "https://www.cabanyesentrevalls.com/wp-content/uploads/2014/12/grevol-750x370.jpg",
         description: "Otro de los hoteles originales en Cataluña que no te puedes perder son las Cabanes als arbres de Dosrius. Es el lugar perfecto para desconectar de la rutina y conectar con el entorno natural. Las cabañas se encuentran en pleno bosque y construidas encima de los árboles. Además, es importante destacar el compromiso de este alojamiento con el medio ambiente, puesto que todas las comodidades que ofrece la cabaña son de lo más sostenibles.",
         showMore: false, 
@@ -51,7 +51,7 @@ export const data = [
     },
     {
         id: 7,        
-        hotel: "Mas Salagros Ecoresort",
+        hotel: "Mas Salagros Ecoresort (Vallromanes)",
         image: "https://www.massalagros.com/wp-content/uploads/2018/05/AIRE-Ancient-Baths-Mas-Salagros.jpg.webp",
         description: "¿Buscas hoteles originales en Cataluña, pero que también sean respetuosos con el medio ambiente? Mas Salagros Ecoresort es uno de los alojamientos con más encanto de Cataluña, pero además, es el primer destino 100% ecológico de la península. Ubicado a tan solo 20 minutos de Barcelona, se encuentra este Ecoresort para desconectar y disfrutar de a filosofía Slow en una reserva natural.En este hotel estuve hace un año y la verdad es que me encantó el concepto. Está ubicado en una finca totalmente natural, en la que dispones de restaurantes y spa 100% ecológicos.",
         showMore: false, 
@@ -59,7 +59,7 @@ export const data = [
     },
     {
         id: 8,        
-        hotel: "Yurta Mongola",
+        hotel: "Yurta Mongola (Delta de l’Ebre)",
         image: "https://static.nationalgeographic.es/files/styles/image_3200/public/deluxe-ger.jpg?w=1900&h=1267",
         description: "¿Te imaginas cómo debe ser dormir en una auténtica Yurta Mongola? Podrás vivir esta sensación en el mismísimo Parque Natural del Delta del Ebro, en uno de los hoteles más singulares de Cataluña. La Yurta es la vivienda típica de los pastores nómadas mongoles. Su estructura cuenta con una cúpula central transparente con la que podrás disfrutar de una noche estrellada y de una estufa de leña para disfrutar de la calidez dentro de la cabaña.",
         showMore: false, 
@@ -67,7 +67,7 @@ export const data = [
     },
     {
         id: 9,        
-        hotel: "Mastinell Cava & Boutique Hotel",
+        hotel: "Mastinell Cava & Boutique Hotel (Vilafranca del Penedès)",
         image: "https://imgcy.trivago.com/c_fill,d_dummy.jpeg,e_sharpen:60,f_auto,h_267,q_40,w_400/hotelier-images/9b/67/f9bc7c06e90691ad88a883d9b3390419e17cfe07bd5012f1fe79377babae.jpeg",
         description: "Si eres un amante del cava y del vino del Penedès, tu hotel diferente en Cataluña es el Mastinell Cava & Boutique Hotel. Su fachada simula la forma de botellas de cava y uno de sus objetivos es la sostenibilidad: el edificio cuenta con paneles solares, tanques para recolectar el agua de la lluvia y calentador de agua de biomasa. Aquí te encontrarás rodeado de viñedos y podrás realizar diferentes actividades como degustaciones y visita de sus bodegas, terapias con vino o alucinar con la comida de su restaurante. Además, ¡cuenta con piscina!",
         showMore: false, 
@@ -75,7 +75,7 @@ export const data = [
     },
     {
         id: 10,        
-        hotel: "Hotel La Vella Farga",
+        hotel: "Hotel La Vella Farga (Lladurs, Solsonès, Lleida)",
         image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/49155715.jpg?k=09a3439504b8ed4a98bb263d3cb6712ae9afa31cc8b37b5ac6dc4d54e255e1a1&o=&hp=1",
         description: "Ubicado muy cerca de Solsona se encuentra este espectacular hotel de Cataluña construido en una masía típica catalana que data del año 1036. La restauración del Hotel La Vella Farga lo ha llevado a convertirse en un alojamiento rural con decoración sofisticada y moderna, pero que conserva su fabulosa fachada y algunos elementos antiguos. Además, cuenta con piscina exterior tipo infinity y está completamente rodeada de naturaleza.",
         showMore: false, 
@@ -83,7 +83,7 @@ export const data = [
     },    
     {
         id: 11,        
-        hotel: "El Far Hotel",
+        hotel: "El Far Hotel (Llafranc, Baix Empordà, Costa Brava)",
         image: "https://cdn.secretplaces.com/images/hotel-media/14-1.jpg",
         description: "Damos un salto a la Costa Brava, concretamente a un acantilado a 175 metros sobre el mar. ¿Y qué tiene de particular El Far Hotel? Pues que se encuentra justo al lado del Far de San Sebastià, uno de los faros más míticos de la Costa Brava. Las 9 habitaciones están decoradas al estilo marinero y cuenta con una terraza espectacular desde la que ver el faro y el mar. ¡Su restaurante es también muy recomendable!",
         showMore: false, 
@@ -91,7 +91,7 @@ export const data = [
     },
     {
         id: 12,        
-        hotel: "El Recer de Masia Serra",
+        hotel: "El Recer de Masia Serra (Cantallops)",
         image: "https://masiaserra.com/wp-content/uploads/2021/04/Habitacio%CC%81-GRANIT-1024x768.jpeg",
         description: "Con terapia de spa, un salón compartido y una zona de pícnic, El Recer de Masia Serra Villa Cantallops disfruta de una ubicación privada, a unos 10 minutos en coche del Dolmen de Fontanilles. Si te quedas aquí, tendrás WiFi en toda la propiedad y un aparcamiento en el lugar.",
         showMore: false, 
@@ -107,10 +107,18 @@ export const data = [
     },
     {
         id: 14,        
-        hotel: "Hotel Esperit Roca",
+        hotel: "Hotel Esperit Roca (Sant Julià de Ramis, Gironès, Girona)",
         image: "https://www.petitsgranshotelsdecatalunya.com/media/hoteles/esperit-roca/proposta-web-hotel-sants-metges-07_web.jpg",
         description: "El Hotel Esperit Roca situado en Sant Julià de Ramis, ofrece vistas panorámicas de 360º hacia Girona y l’Empordà. Su cercanía a Girona y su oferta de actividades al aire libre lo convierten en un lugar ideal tanto para una escapada corta como para una estancia más prolongada. Este hotel de 15 habitaciones, construidas con materiales naturales, son concebidas para armonizarse con su entorno y brindar un espacio exclusivo y apacible. Creadas para ser estancias sumamente confortables, donde podrán conocer su nuevo restaurante gastronómico, descubrir su destilería y explorar su última exposición.",
         showMore: false, 
         sourse: "https://hotelesperitroca.com/"
+    },
+    {
+        id: 15,        
+        hotel: "Hotel La Plaça de Madremanya (Madremanya, Gironès, Girona)",
+        image: "https://hotel-la-placa-madremanya-17462.topcostabravahotels.com/data/Photos/OriginalPhoto/4630/463068/463068675/hotel-la-placa-madremanya-photo-47.JPEG",
+        description: "Entre Girona i el Ampurdán hay un pequeño paraíso, ondulado y benigno. Madremanya es el corazón. Es un pueblo de aire italiano, situado sobre una colina y con una iglesia de color de miel, que a su vez es el color de las casas y las calles, donde el tiempo, dormido, descansa. Joya de este paraíso es el restaurante La Plaça, de cocina refinada y sutil, convertido en un hotelito encantador, rodeado de un jardín aromático, abierto a Les Gavarres siempre verdes, maternales, silenciosas.",
+        showMore: false, 
+        sourse: "https://www.hotelmadremanya.com/"
     },
 ]
